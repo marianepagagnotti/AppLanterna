9a906e50969834094d76e506588d39cae6114ea8
Usuario <Usuario@CPD02-M19>
2022-06-22 14:42:42 -0300

    Adicionar arquivos de projeto.

---
+++ App_Lanterna/App_Lanterna.Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+
+namespace App_Lanterna.Droid
+{
+    [Activity(Label = "App_Lanterna", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+
+            /**
+             * Usar recursos que ja estão implementados na Xamarim.Essential no App Android
+             * (dando permissão)
+             */
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            Window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+            Window.AddFlags(Android.Views.WindowManagerFlags.DrawsSystemBarBackgrounds);
+            Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#232323"));
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
+}
---
+++ App_Lanterna/App_Lanterna/MainPage.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Battery;
+using Xamarin.Essentials;
+
+namespace App_Lanterna
+{
+    public partial class MainPage : ContentPage
+    {
+        bool lanterna_ligada = false;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnOnOff.Source = ImageSource.FromResource("App_Lanterna.Imagem.off.PNG");
+           
+
+            Carrega_Informacoes_Bateria();
+
+        }
+
+
+        private async void Carrega_Informacoes_Bateria()
+        {
+            try
+            {
+                /** 
+                 * Verificando se o plugin é suportado pelo dispositivo. Caso contrário uma
+                 * mensagem é exibida ao usuário avisando que as informações não estão disp
+                 */
+
+                if (CrossBattery.IsSupported)
+                {
+                    CrossBattery.Current.BatteryChanged -= Mudanca_Status_Bateria;
+                    CrossBattery.Current.BatteryChanged += Mudanca_Status_Bateria;
+                }
+                else
+                {
+                    lbl_bateria_fraca.Text = "As informações sobre a bateria não esão disponíveis :(";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ocorreu um erro: \n", ex.Message, "OK");
+            }
+
+        }
+
+        private async void Mudanca_Status_Bateria(object sender, Plugin.Battery.Abstractions.BatteryChangedEventArgs e)
+        {
+            try
+            {
+                // Mostrando a carga restante da bateria em porcentagem.
+                lbl_porcentagem_restante.Text = e.RemainingChargePercent.ToString() + "%";
+
+                /** 
+                 * Caso a bateria esteja fraca, mostra uma mensagem para o usuário
+                 */
+                if (e.IsLow)
+                {
+                    lbl_bateria_fraca.Text = "Atenção!!! A Bateria Está Fraca!!";
+                }
+                else
+                {
+                    lbl_bateria_fraca.Text = "";
+                }
+
+                /**
+                 * Estrutura (escolha caso) para mostrar qual é o status da bateria: Carregando, 
+                 * descarregando, etc. 
+                 * O argumento e carrega as mudanças acontecidas na bateria, que pode ter sido colocada
+                 * para carregar ou estar descarregando. Dentro desse argumento podemos acessar várias informações 
+                 * como o satus da bateria, e tratamos para a mensagem ficar mais amigavel para o usuário
+                 */
+
+                switch (e.Status)
+                {
+                    case Plugin.Battery.Abstractions.BatteryStatus.Charging:
+                        lbl_status.Text = "Carregando";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.Discharging:
+                        lbl_status.Text = "Descarregando";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.Full:
+                        lbl_status.Text = "Carregada";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.NotCharging:
+                        lbl_status.Text = "Sem Carregar";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.Unknown:
+                        lbl_status.Text = "Desconhecido";
+                        break;
+                }
+
+                //Mostra a fonte de energia atual do dispositivo
+
+                switch (e.PowerSource)
+                {
+                    case Plugin.Battery.Abstractions.PowerSource.Ac:
+                        lbl_fonte_carregamento.Text = "Carregador";
+                        break;
+
+                    case Plugin.Battery.Abstractions.PowerSource.Battery:
+                        lbl_fonte_carregamento.Text = "Bateria";
+                        break;
+
+                    case Plugin.Battery.Abstractions.PowerSource.Usb:
+                        lbl_fonte_carregamento.Text = "USB";
+                        break;
+
+                    case Plugin.Battery.Abstractions.PowerSource.Wireless:
+                        lbl_fonte_carregamento.Text = "Sem Fio";
+                        break;
+
+                    case Plugin.Battery.Abstractions.PowerSource.Other:
+                        lbl_fonte_carregamento.Text = "Desconhecida";
+                        break;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ocorreu um erro: \n", ex.Message, "OK");
+            }
+        }
+        private async void btnOnOff_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!lanterna_ligada)
+                {
+                    lanterna_ligada = true;
+
+                    btnOnOff.Source = ImageSource.FromResource("App_Lanterna.Imagem.on.PNG");
+
+                    Vibration.Vibrate(TimeSpan.FromMilliseconds(250));
+
+                    await Flashlight.TurnOnAsync();
+                }
+                else
+                {
+                    lanterna_ligada = false;
+
+                    btnOnOff.Source = ImageSource.FromResource("App_Lanterna.Imagem.off.PNG");
+
+                    Vibration.Vibrate(TimeSpan.FromMilliseconds(250));
+
+                    await Flashlight.TurnOffAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ocorreu um erro: \n", ex.Message, "OK");
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
